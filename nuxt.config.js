57f355a25c032374fd1556b944adfb2ccb424427
Brahyan Rodriguez <45783288+Brahyanr10@users.noreply.github.com>
2021-06-02 13:11:54 -0500

    Landing

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,27 +1,30 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: "I-y-A",
+    title: 'I-y-A',
     htmlAttrs: {
-      lang: "en"
+      lang: 'en'
     },
     meta: [
-      { charset: "utf-8" },
-      { name: "viewport", content: "width=device-width, initial-scale=1" },
-      { hid: "description", name: "description", content: "" }
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' }
     ],
-    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
   },
-  css: ["element-ui/lib/theme-chalk/index.css", "swiper/dist/css/swiper.css"],
+  css: ['element-ui/lib/theme-chalk/index.css', 'swiper/dist/css/swiper.css'],
   plugins: [
-    "~/plugins/element",
-    { src: "~/plugins/typed.js", ssr: true },
-    { src: "~/plugins/countryFlags.js", ssr: false },
-    { src: "~/plugins/vue-awesome-swiper.js", ssr: false },
-    { src: "@/plugins/i18n" }
+    '~/plugins/element',
+    { src: '~/plugins/typed.js', ssr: true },
+    { src: '~/plugins/countryFlags.js', ssr: false },
+    { src: '~/plugins/vue-awesome-swiper.js', ssr: false },
+    { src: '@/plugins/i18n' }
   ],
   components: true,
-  buildModules: ["@nuxtjs/tailwindcss"],
+  buildModules: ['@nuxtjs/tailwindcss'],
   modules: [],
-  build: {}
-};
+  build: {},
+  generate: {
+    fallback: true
+  }
+}
